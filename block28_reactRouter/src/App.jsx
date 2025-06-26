024ff4bf93eb60bfaf4006b186a4e259ae3f1322
jshirey55 <jshirey5555@gmail.com>
2025-06-25 20:28:11 -0400

    start

---
+++ block28_reactRouter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
+import Blue from './components/Blue'
+import Red from './components/Red'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+     <div id="container">
+      <h1>Hello React Router!</h1>
+      <div id="navbar">
+        <Link to='/blue'>BLUE!</Link>
+        <Link to='/red'>RED!</Link>
+        <Link to='/'>HOME!</Link>
+      </div>
+      <div id="main-section"><div id="main-section">
+          <Routes>
+            <Route path="/blue" element={<Blue/>} />
+            <Route path="/red" element={<Red/>} />
+            <Route path='/' element={<Home/>} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App
